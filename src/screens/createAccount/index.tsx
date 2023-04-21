906e55eb7f3fc721323ae062d4cc3aae1a0780bd
Luan Emanuel da Silva <programar@luanemanuel.com.br>
2023-04-21 15:07:51 -0300

    Criação da view de cadastro

--- src/screens/createAccount/index.tsx
+++ src/screens/createAccount/index.tsx
@@ -1,11 +1,117 @@
 import React, {useState, useEffect} from "react";
-import {Container} from "./styles";
+import {
+    ConfirmFieldView,
+    ConfirmText,
+    ConfirmTextField,
+    Container,
+    Content,
+    DateButton,
+    DateField,
+    DateFieldView,
+    DateText,
+    DateTextContent,
+    EmailFieldView,
+    EmailText,
+    EmailTextField,
+    GenderFieldView,
+    GenderText,
+    NameFieldView,
+    NameText,
+    NameTextField,
+    PassFieldView,
+    PassText,
+    PassTextField,
+    RegisterButton, RegisterButtonText,
+    RegisterButtonView,
+    WrongPassText,
+    WrongPassView
+} from "./styles";
 import VaccineAppBar from "../../components/VaccineAppBar";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import moment from "moment";
+
+import Calendar from "../../assets/calendar.svg";
+import DatePicker from "react-native-date-picker";
 
 function CreateAccount() {
+    const [date, setDate] = useState(new Date());
+    const [openPicker, setOpenPicker] = useState(false);
+
     return (
         <Container>
             <VaccineAppBar haveDrawer={false} text="My Health"/>
+            <Content>
+                <NameFieldView>
+                    <NameText>Nome Completo</NameText>
+                    <NameTextField/>
+                </NameFieldView>
+                <GenderFieldView>
+                    <GenderText>Sexo</GenderText>
+                    <BouncyCheckbox size={24}
+                                    unfillColor="#fff"
+                                    fillColor="#419ED7"
+                                    innerIconStyle={{
+                                        borderColor: "#fff",
+                                        borderWidth: 3,
+                                        borderStyle: 'solid',
+                                    }}
+                                    iconImageStyle={{opacity: 0}}
+                                    style={{paddingLeft: 15}}/>
+                    <GenderText>Masculino</GenderText>
+                    <BouncyCheckbox size={24}
+                                    unfillColor="#fff"
+                                    fillColor="#419ED7"
+                                    innerIconStyle={{
+                                        borderColor: "#fff",
+                                        borderWidth: 3,
+                                        borderStyle: 'solid',
+                                    }}
+                                    iconImageStyle={{opacity: 0}}
+                                    style={{paddingLeft: 15}}/>
+                    <GenderText>Feminino</GenderText>
+                </GenderFieldView>
+                <DateFieldView>
+                    <DateText>Data nascimento</DateText>
+                    <DateField>
+                        <DateButton onPress={() => setOpenPicker(true)}>
+                            <DateTextContent>{moment(date).format('DD/MM/YYYY')}</DateTextContent>
+                            <Calendar height={20} width={20} style={{paddingRight: 30}}/>
+                            <DatePicker modal
+                                        date={date}
+                                        open={openPicker}
+                                        onConfirm={(date) => {
+                                            setOpenPicker(false)
+                                            console.log(date)
+                                            setDate(date)
+                                        }} onCancel={() => {
+                                setOpenPicker(false)
+                            }} mode="date" />
+                        </DateButton>
+                    </DateField>
+                </DateFieldView>
+                <EmailFieldView>
+                    <EmailText>E-mail</EmailText>
+                    <EmailTextField/>
+                </EmailFieldView>
+                <PassFieldView>
+                    <PassText>Senha</PassText>
+                    <PassTextField/>
+                </PassFieldView>
+                <ConfirmFieldView>
+                    <ConfirmText>Repetir senha</ConfirmText>
+                    <ConfirmTextField/>
+                </ConfirmFieldView>
+                <WrongPassView>
+                    <WrongPassText>Senha não confere!</WrongPassText>
+                </WrongPassView>
+                <RegisterButtonView>
+                    <RegisterButton>
+                        <RegisterButtonText>
+                            Cadastrar
+                        </RegisterButtonText>
+                    </RegisterButton>
+                </RegisterButtonView>
+            </Content>
         </Container>
     );
 }
--- src/screens/createAccount/styles.tsx
+++ src/screens/createAccount/styles.tsx
@@ -2,8 +2,209 @@ import React from "react";
 import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
-  align-items: center;
+  align-items: flex-end;
   background: #ADD4D0;
   height: 100%;
   width: 100%;
+`
+
+export const Content = styled.View`
+  align-items: flex-end;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+`;
+
+export const NameFieldView = styled.View`
+  padding-top: 100px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const NameTextField = styled.TextInput`
+  width: 60%;
+  height: 35px;
+  background: #fff;
+  margin-left: 10px;
+  color: #419ED7;
+  font-family: 'AveriaLibre-Regular';
+  font-size: 18px;
+`;
+
+export const NameText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const GenderFieldView = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const GenderText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const DateFieldView = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const DateField = styled.View`
+  width: 58%;
+  height: 35px;
+  background: #fff;
+  margin-left: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DateTextContent = styled.Text`
+  background: #fff;
+  margin-left: 10px;
+  color: #419ED7;
+  font-family: 'AveriaLibre-Regular';
+  font-size: 18px;
+`;
+
+export const DateText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const DateButton = styled.TouchableOpacity`
+  width: 100%;
+  background: transparent;
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const EmailFieldView = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const EmailTextField = styled.TextInput`
+  width: 83%;
+  height: 35px;
+  background: #fff;
+  margin-left: 10px;
+  color: #419ED7;
+  font-family: 'AveriaLibre-Regular';
+  font-size: 18px;
+`;
+
+export const EmailText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const PassFieldView = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const PassTextField = styled.TextInput`
+  width: 83%;
+  height: 35px;
+  background: #fff;
+  margin-left: 10px;
+  color: #419ED7;
+  font-family: 'AveriaLibre-Regular';
+  font-size: 18px;
+`;
+
+export const PassText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const ConfirmFieldView = styled.View`
+  padding-top: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ConfirmTextField = styled.TextInput`
+  width: 65%;
+  height: 35px;
+  background: #fff;
+  margin-left: 10px;
+  color: #419ED7;
+  font-family: 'AveriaLibre-Regular';
+  font-size: 18px;
+`;
+
+export const ConfirmText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const WrongPassView = styled.View`
+    width: 98%;
+`;
+
+export const WrongPassText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+  color: #FD7979;
+`;
+
+export const RegisterButtonView = styled.View`
+    padding-top: 200px;
+    align-items: center;
+    width: 100%;
+`;
+
+export const RegisterButton = styled.TouchableOpacity`
+  width: 40%;
+  height: 40px;
+  background: #37BD6D;
+  align-items: center;
+  justify-content: center;
+  elevation: 10;
+`;
+
+export const RegisterButtonText = styled.Text`
+  font-family: 'AveriaLibre-Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  text-align: center;
+  padding: 5px 25px 5px 25px;
+  color: #fff;
 `;
